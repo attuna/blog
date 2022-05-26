567839be2fc2628c0173b31b030b118f2b706274
attuna <peshko0003@gmail.com>
2022-05-26 15:43:16 +0300

    add models and forms

--- blog/myblog/models.py
+++ blog/myblog/models.py
@@ -1,3 +1,46 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+STATUS = (
+    (0, "Draft"),
+    (1, "Publish"),
+    (2, "Archive"),
+)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=64, unique=True)
+    content = models.TextField()
+    status = models.IntegerField(choices=STATUS, default=0)
+    image = models.ImageField(
+        upload_to="post",
+        default="post/sample.jpg",
+    )
+    author = models.ForeignKey(
+        User,
+        related_name="blog_posts",
+        on_delete=models.CASCADE,
+    )
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Comment(models.Model):
+    post = models.ForeignKey(
+        Post, related_name="comments", on_delete=models.CASCADE
+    )
+    author = models.ForeignKey(
+        User,
+        related_name="comments",
+        on_delete=models.CASCADE,
+    )
+    text = models.TextField()
+    approve_comment=models.BooleanField(default=False)
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.text

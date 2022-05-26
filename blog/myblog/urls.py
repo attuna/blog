72614c01b868577fc0caad1766f5e6796e4eb643
attuna <peshko0003@gmail.com>
2022-05-26 21:43:12 +0300

    AFTER 2.5 HOURS THIS F**** SHIT IS WORKING (ADD post form)

--- blog/myblog/urls.py
+++ blog/myblog/urls.py
@@ -4,5 +4,7 @@ from . import views
 urlpatterns = [
     path("", views.PostList.as_view(), name="post_list"),
     path("user/new/", views.CreateUser.as_view(), name="new_user"),
+    path("post/new", views.CreatePost.as_view(), name="post_new"),
+
 
 ]
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -4,10 +4,32 @@ from django.views import generic
 from django.contrib.auth.forms import UserCreationForm
 from .models import Comment, Post
 from django.contrib.auth.models import User
+from django.contrib.auth.mixins import LoginRequiredMixin
+from .forms import CommentForm,PostForm
 
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
+
+
+class CreatePost(LoginRequiredMixin, generic.CreateView):
+    login_url = reverse_lazy("login")
+    success_url = reverse_lazy("post_list")
+
+    form_class = PostForm
+    queryset = Post.objects.all()
+    template_name = "myblog/post_form.html"
+
+    def post(self, request, *args, **kwargs):
+        request.POST._mutable = True
+        request.POST["author"] = request.user
+        request.POST._mutable = False
+
+        form = self.get_form()
+        if form.is_valid():
+            return self.form_valid(form)
+        else:
+            return self.form_invalid(form)
 
 
 class CreateUser(generic.CreateView):

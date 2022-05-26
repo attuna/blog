3a82dd77d7d6b529a38fd23edab2629d617b06bd
attuna <peshko0003@gmail.com>
2022-05-26 19:41:49 +0300

    add sing up && bootstrap styles

--- blog/myblog/urls.py
+++ blog/myblog/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.PostList.as_view(), name="post_list")
+    path("", views.PostList.as_view(), name="post_list"),
+    path("user/new/", views.CreateUser.as_view(), name="new_user"),
 
 ]
--- blog/myblog/views.py
+++ blog/myblog/views.py
@@ -1,7 +1,17 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
 from django.views import generic
+from django.contrib.auth.forms import UserCreationForm
 from .models import Comment, Post
+from django.contrib.auth.models import User
 
 
 class PostList(generic.ListView):
     queryset = Post.objects.filter(status=1).order_by("-created_on")
+
+
+class CreateUser(generic.CreateView):
+    success_url = reverse_lazy("post_list")
+    form_class = UserCreationForm
+    queryset = User.objects.all()
+    template_name = "registration/signup.html"
